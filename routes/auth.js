60fdcbf85328a650af1f68b758b8910b4de88226
moriMaroo17 <morimaroo17@gmail.com>
2021-09-08 02:02:30 +0300

    complete password recovery feature

--- routes/auth.js
+++ routes/auth.js
@@ -90,6 +90,32 @@ router.get('/reset', (req, res) => {
     })
 })
 
+router.get('/password/:token', async (req, res) => {
+    if (!req.params.token) {
+        return res.redirect('/auth/login')
+    }
+
+    try {
+        const user = await User.findOne({
+            resetToken: req.params.token,
+            resetTokenExp: {$gt: Date.now()}
+        })
+
+        if (!user) {
+            return redirect('/auth/login')
+        } else {
+            res.render('auth/password', {
+                title: 'Восстановить доступ',
+                error: req.flash('error'),
+                userId: user._id.toString(),
+                token: req.params.token
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 router.post('/reset', (req, res) => {
     try {
         crypto.randomBytes(32, async (err, buffer) => {
@@ -112,6 +138,29 @@ router.post('/reset', (req, res) => {
                 res.redirect('/auth/reset')
             }
         })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.post('/password', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            _id: req.body.userId,
+            resetToken: req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        })
+
+        if (user) {
+            user.password = await bcrypt.hash(req.body.password, 10)
+            user.resetToken = undefined
+            user.resetTokenExp = undefined
+            await user.save()
+            res.redirect('/auth/login')
+        } else {
+            req.flash('loginError', 'Время жизни токена истекло')
+            res.redirect('/auth/login')
+        }
     } catch (error) {
         console.log(error)
     }
