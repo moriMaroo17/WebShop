1f940d506aefc957f3ca15f687ec6bea2326dc12
moriMaroo17 <morimaroo17@gmail.com>
2021-09-14 20:14:16 +0300

    added sanitizers

--- utils/validators.js
+++ utils/validators.js
@@ -2,24 +2,33 @@ import { body } from 'express-validator/check/index.js'
 import User from '../models/user.js'
 
 const registerValidators = [
-    body('email').isEmail().withMessage('Введите корректный email').custom(async (value, {req}) => {
-        try {
-            const user = await User.findOne({ email: value })
-            if (user) {
-                return Promise.reject('Такой email уже занят')
+    body('email')
+        .isEmail().withMessage('Введите корректный email')
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({ email: value })
+                if (user) {
+                    return Promise.reject('Такой email уже занят')
+                }
+            } catch (error) {
+                console.log(error)
             }
-        } catch (error) {
-            console.log(error)
-        }
-    }),
-    body('password', 'Введите пароль длинной 6-56 символов из латинских букв и цифр').isLength({min: 6, max: 56}).isAlphanumeric(),
-    body('confirm').custom((value, {req}) => {
-        if (value !== req.body.password) {
-            throw new Error('Пароли должны совпадать')
-        }
-        return true
-    }),
+        })
+        .normalizeEmail(),
+    body('password', 'Введите пароль длинной 6-56 символов из латинских букв и цифр')
+        .isLength({min: 6, max: 56})
+        .isAlphanumeric()
+        .trim(),
+    body('confirm')
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Пароли должны совпадать')
+            }
+            return true
+        })
+        .trim(),
     body('name').isLength({min: 3}).withMessage('Имя должно быть минимум 3 символа')
+        .trim()
 ]
 
 export { registerValidators }
